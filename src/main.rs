81ecf783f2dcdebf7b8ac63c2ad5a61dc42be1f1
kenmori <kenjimoritata@gmail.com>
2021-09-12 09:22:16 +0900

    fix: text

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,8 @@ pub fn count(input: impl BufRead) -> HashMap<String, usize> {
 
   for line in input.lines() {
     let line = line.unwrap();
-
     for m in re.find_iter(&line) {
       let word = m.as_str().to_string();
-
       *freqs.entry(word).or_insert(0) += 1;
     }
   }
